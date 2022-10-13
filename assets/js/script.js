5fc389e7ba6b1d9190cc23ceb68faf8a95a47b83
Globulo <109812198+GlobuloBianco@users.noreply.github.com>
2022-10-13 15:59:46 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,31 +21,29 @@ document.getElementById('modificato').innerHTML = umani; //splice
 
 //---[3]------[3]------[3]------[3]------[3]------[3]------[3]------[3]------[3]---//
 const anno = 2022;
+const liNascita = [2001, 1990, 1987, 2018, 2010]; //arrayList
 
 calcoloEta = (annoNascita) => {
     let miaEta = anno - annoNascita;
     return miaEta;
 }
-document.getElementById('eta1').innerHTML += `${calcoloEta(2001)} anni`;
-document.getElementById('eta2').innerHTML += `${calcoloEta(1990)} anni`;
-document.getElementById('eta3').innerHTML += `${calcoloEta(1987)} anni`;
-document.getElementById('eta4').innerHTML += `${calcoloEta(2018)} anni`;
-document.getElementById('eta5').innerHTML += `${calcoloEta(2010)} anni`;
+document.getElementById('eta1').innerHTML += `${calcoloEta(liNascita[0])} anni`;
+document.getElementById('eta2').innerHTML += `${calcoloEta(liNascita[1])} anni`;
+document.getElementById('eta3').innerHTML += `${calcoloEta(liNascita[2])} anni`;
+document.getElementById('eta4').innerHTML += `${calcoloEta(liNascita[3])} anni`;
+document.getElementById('eta5').innerHTML += `${calcoloEta(liNascita[4])} anni`;
 //Creato un array con l'eta calcolata e convertita in stringa
-const etaTot = [calcoloEta(2001), calcoloEta(1990), calcoloEta(1987), calcoloEta(2018), calcoloEta(2010)];
+const etaTot = [calcoloEta(liNascita[0]), calcoloEta(liNascita[1]), calcoloEta(liNascita[2]), calcoloEta(liNascita[3]), calcoloEta(liNascita[4])];
 document.getElementById('arrayEta').innerHTML += etaTot.toString();
 
 //---[4]------[4]------[4]------[4]------[4]------[4]------[4]------[4]------[4]---//
 const animali = ['coniglio','cane','gatto','criceto'];
 
 document.getElementById('intero').innerHTML = animali; //arrayList
-
 var sonoInutile = animali.push('leone');
 document.getElementById('aggiunto').innerHTML = animali; // push
-
 var sonoInutile = animali.pop('leone')
 document.getElementById('estratto').innerHTML = animali; // pop
-
 document.getElementById('verifica1').innerHTML = animali.includes('criceto'); //true
 document.getElementById('verifica2').innerHTML = animali.includes('Qualora ti sentissi inutile e depresso, ricordati che un giorno sei stato lo spermatozoo più veloce di tutti.');
 //false
